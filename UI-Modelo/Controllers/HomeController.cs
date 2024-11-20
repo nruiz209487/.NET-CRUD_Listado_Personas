2f694f9374ce5adf29f65495987f03225ac593c6
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-20 14:42:20 +0100

    ss

--- UI-Modelo/Controllers/HomeController.cs
+++ UI-Modelo/Controllers/HomeController.cs
@@ -1,32 +1,87 @@
+﻿using BL;
+using ENT_Modelo;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using UI_Modelo.Models;
 
 namespace UI_Modelo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        // GET: HomeController
+        public ActionResult Index()
         {
-            _logger = logger;
+            List<ClsPersona> list = ListaPersonasBL.listadoPersonas;
+            return View(list);
         }
 
-        public IActionResult Index()
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id)
+        {
+   
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
