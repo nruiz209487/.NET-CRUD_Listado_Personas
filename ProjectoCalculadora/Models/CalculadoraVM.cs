417f5e0b623bbc2fb8b0ff586b834503d7bbcc02
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-27 14:20:53 +0100

    ss

--- ProjectoCalculadora/Models/CalculadoraVM.cs
+++ ProjectoCalculadora/Models/CalculadoraVM.cs
@@ -11,9 +11,24 @@ namespace ProjectoCalculadora.Models
 {
     public class CalculadoraVM : INotifyPropertyChanged
     {
+        /// <summary>
+        /// variable para hacer operaciones
+        /// </summary>
         private double _OperadorX;
+        /// <summary>
+        /// variable para hacer operaciones
+        /// </summary>
         private double _OperadorY;
+        /// <summary>
+        /// Resultado de la operacion
+        /// </summary>
         private double _Resultado;
+
+
+
+        /// <summary>
+        /// Enlace entre la variable privada y el get y set que sirve para manejar el valor del propertychange
+        /// </summary>
         public double OperadorX
         {
             get => _OperadorX;
@@ -22,10 +37,14 @@ namespace ProjectoCalculadora.Models
                 if (_OperadorX != value)
                 {
                     _OperadorX = value;
-                    OnPropertyChanged(nameof(OperadorX)); 
+                    OnPropertyChanged(nameof(OperadorX));
                 }
             }
         }
+
+        /// <summary>
+        /// Enlace entre la variable privada y el get y set que sirve para manejar el valor del propertychange
+        /// </summary>
         public double OperadorY
         {
             get => _OperadorY;
@@ -34,10 +53,14 @@ namespace ProjectoCalculadora.Models
                 if (_OperadorY != value)
                 {
                     _OperadorY = value;
-                    OnPropertyChanged(nameof(OperadorY)); 
+                    OnPropertyChanged(nameof(OperadorY));
                 }
             }
         }
+
+        /// <summary>
+        /// Enlace entre la variable privada y el get y set que sirve para manejar el valor del propertychange
+        /// </summary>
         public double Resultado
         {
             get => _Resultado;
@@ -46,50 +69,87 @@ namespace ProjectoCalculadora.Models
                 if (_Resultado != value)
                 {
                     _Resultado = value;
-                    OnPropertyChanged(nameof(Resultado)); 
+                    OnPropertyChanged(nameof(Resultado));
                 }
             }
         }
-    
+        /// <summary>
+        /// Lista de comandos para cada funcion
+        /// </summary>
         public ICommand Suma { get; }
         public ICommand Resta { get; }
         public ICommand Producto { get; }
         public ICommand Division { get; }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public ICommand Potencia { get; }
+        public ICommand Resto { get; }
 
 
+
+        /// <summary>
+        /// Contructor de la clase inicializa los command (haven referencia a la funcion)
+        /// </summary>
         public CalculadoraVM()
         {
             Suma = new Command(OperacionSuma);
             Resta = new Command(OpeacionResta);
             Producto = new Command(OperacionProducto);
             Division = new Command(OperacionDivision);
+            Potencia = new Command(OperacionPotencia);
+            Resto = new Command(OperacioResto);
         }
-
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion division
+        /// </summary>
         private void OperacionDivision()
         {
             Resultado = ClsCalculadora.division(OperadorX, OperadorY);
         }
-
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion producto
+        /// </summary>
         private void OperacionProducto()
         {
             Resultado = ClsCalculadora.producto(OperadorX, OperadorY);
         }
-
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion resta
+        /// </summary>
         private void OpeacionResta()
         {
             Resultado = ClsCalculadora.resta(OperadorX, OperadorY);
         }
-
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion suma
+        /// </summary>s
         private void OperacionSuma()
         {
             Resultado = ClsCalculadora.suma(OperadorX, OperadorY);
+        }
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion potencia
+        /// </summary>
+        private void OperacionPotencia()
+        {
+            Resultado = ClsCalculadora.potencia(OperadorX, OperadorY);
+        }
+
+
+        /// <summary>
+        /// llama a la interfaz y devuelve el resultado de una operacion resta
+        /// </summary>
+        private void OperacioResto()
+        {
+            Resultado = ClsCalculadora.resto(OperadorX, OperadorY);
+        }
+
+        /// <summary>
+        /// implementacion INotifyPropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
--- ProjectoCalculadora/Models/ClsCalculadora.cs
+++ ProjectoCalculadora/Models/ClsCalculadora.cs
@@ -26,6 +26,19 @@ namespace ProjectoCalculadora.Models
         {
             return x / y;
         }
+
+        public static double potencia(double x, double y)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                x *= x;
+            }
+            return x;
+        }
+        public static double resto(double x, double y)
+        {
+            return x % y;
+        }
     }
 
 
